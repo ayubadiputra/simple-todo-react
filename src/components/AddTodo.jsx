9cb592220a257f6e37715b51e827a83987f7d9db
Ayub Adiputra <ayubna32@gmail.com>
2018-03-26 23:44:15 +0700

    Styling main container app.

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -5,7 +5,7 @@ class MTAddTodo extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <h1 className={styles.header}>What do you want to do today?</h1>
+        <h2 className={styles.header}>What do you want to do today?</h2>
         <form className={styles.form}>
           <input type="text" name="task" className={styles.input} />
         </form>
