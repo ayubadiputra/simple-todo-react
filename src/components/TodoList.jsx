65c473e375178cc6176f2cfa15c6a31ddedc803b
Ayub Adiputra <ayubna32@gmail.com>
2018-03-27 22:45:27 +0700

    Update how to sort tasks based on task status

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,13 +5,26 @@ import styles from './TodoList.scss';
 class MTTodoList extends Component {
   render() {
     const tasks = this.props.tasks;
+    const active = [];
+    const completed = [];
+
+    tasks.forEach( task => {
+      if ( task.active ) {
+        active.push(
+          <MTTodoItem key={task.id} task={task} completeTask={this.props.completeTask} />
+        );
+      } else {
+        completed.push(
+          <MTTodoItem key={task.id} task={task} completeTask={this.props.completeTask} />
+        );
+      }
+    } );
 
     return (
       <div className="mt-todolist">
         <ul className="mt-todolist__list">
-          { tasks.map( (task) =>
-              <MTTodoItem key={task.id} task={task} completeTask={this.props.completeTask} />
-          ) }
+          { active }
+          { completed }
         </ul>
         <div className="mt-todolist__filters">
           <a id="all" href="#all">All</a>
